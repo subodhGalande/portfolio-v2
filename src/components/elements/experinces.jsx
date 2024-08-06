c1c61c1d9c3e2bca676af0e1dd52fd6336327285
Subodh Galande <subodh.dsgn@gmail.com>
2024-08-06 22:37:30 +0530

    fix: experience timeline element changed

--- src/components/elements/experinces.jsx
+++ src/components/elements/experinces.jsx
@@ -1,31 +1,9 @@
 import Timeline from "../Timeline";
 
 const Experience = () => {
-  const experiences = [
-    {
-      title: "Software Engineer",
-      company: "Tech Company A",
-      duration: "Jan 2020 - Present",
-      description: "Developed web applications using React and Node.js.",
-    },
-    {
-      title: "Frontend Developer",
-      company: "Startup B",
-      duration: "Jun 2018 - Dec 2019",
-      description: "Built responsive websites with HTML, CSS, and JavaScript.",
-    },
-    {
-      title: "Intern",
-      company: "Company C",
-      duration: "Jan 2018 - May 2018",
-      description:
-        "Assisted in the development of internal tools using Python.",
-    },
-  ];
-
   return (
     <>
-      <div className="flex flex-col justify-between text-grey mt-36">
+      <div className="flex mx-auto flex-col justify-between text-grey mt-36">
         <h1 className="text-4xl text-center max-w-lg mx-auto font-bold uppercase font-heading leading-9 tracking-tight">
           SHAPED BY MY JOURNEY, NEVER FORGETTING WHERE I STARTED.
         </h1>
@@ -33,7 +11,9 @@ const Experience = () => {
           Designing wasn’t a linear career; these were my stepping stones.
         </p>
       </div>
-      <Timeline />
+      <div className="mx-auto w-10/12">
+        <Timeline />
+      </div>
     </>
   );
 };

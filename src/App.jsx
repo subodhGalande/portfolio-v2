ed885fa903acec8f8e27e33493cd8273251edf70
Subodh Galande <subodh.dsgn@gmail.com>
2024-08-10 15:06:12 +0530

    feat: starry background

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,23 @@ import Projects from "./components/Projects";
 import Experience from "./components/elements/experinces";
 import Footer from "./components/Footer";
 import Techstack from "./components/Tech";
+import Animation from "./components/Background";
+import { ShootingStars } from "./components/ui/shooting-stars";
+import { StarsBackground } from "./components/ui/stars-background";
 
 function App() {
   return (
     <>
-      <Navbar />
-      <div className=" mx-auto px-7">
+      <div className="h-screen rounded-md bg-neutral-900 flex flex-col items-center justify-center relative w-full">
+        <Navbar />
         <Hero />
         <Techstack />
         <Projects />
         <Experience />
+        <Footer />
+        <ShootingStars />
+        <StarsBackground />
       </div>
-      <Footer />
     </>
   );
 }

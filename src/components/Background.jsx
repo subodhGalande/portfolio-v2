52baf0ef65c5684538a31fe004f6130e9e3b4a67
subodh galande <subodh.dev@outlook.com>
2024-10-01 12:35:03 +0530

    fix: bg animation

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -2,9 +2,9 @@ const Animation = () => {
   return (
     <>
       <div className=" w-full fixed z-0 h-screen overflow-hidden blur-3xl ">
-        <div className=" absolute w-96 h-96  bg-grey blur-3xl opacity-30 rounded-full  animate-rotate-move-1  mix-blend-multiply "></div>
-        <div className="absolute w-80 h-80  bg-grey blur-3xl  opacity-25 rounded-full animate-rotate-move-2 mix-blend-multiply "></div>
-        <div className="absolute w-64 h-64  bg-grey  blur-3xl opacity-30 rounded-full animate-rotate-move-3  mix-blend-multiply"></div>
+        <div className=" absolute w-96 h-96  bg-grey blur-2xl  opacity-15 rounded-full  animate-rotate-move-1  "></div>
+        <div className="absolute w-80 h-80  bg-grey blur-2xl  opacity-15 rounded-full animate-rotate-move-2 "></div>
+        <div className="absolute w-64 h-64  bg-grey blur-2xl  opacity-15 rounded-full animate-rotate-move-3 "></div>
       </div>
     </>
   );
